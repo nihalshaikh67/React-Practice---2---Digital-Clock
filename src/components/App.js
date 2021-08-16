18449dc4cc999cc24ab4d871e55011d38a3f604c
nihalshaikh67 <nihalshaikh834@gmail.com>
2021-08-16 17:32:11 +0530

    ad

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,35 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
 
-        return(
-            <>
-               
-            </>
-        )
+    constructor(){
+        super();
+        this.state ={
+            curTime  :new Date().toLocaleDateString()
+        }
     }
-}
+            componentDidMount() {
+                this.timer = setInterval ( ()=>{
+                    this.setState({
+                        curTime : new Date().toLocaleTimeString()
+                    })
+                },1000)
+            }
+
+                componentWillUnmount(){
+                    clearInterval(this.timer)
+                }
+                  render() {
+
+                return(
+                    <>
+                    <div className="Clock">
+                        <h3 id="time">{this.state.curTime}</h3>
+                        </div>  
+                    </>
+                )
+        }
+    }
 
 
 export default App;
